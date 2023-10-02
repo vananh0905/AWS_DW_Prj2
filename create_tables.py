d1e3352f873dcc2c2de5e2bd135de2ec815398c3
vananh0905 <vananh09052000@gmail.com>
2023-10-02 16:25:34 +0700

    add docstring

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,14 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    # Execute all drop table queries.
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    # Execute all create table queries.
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- etl.py
+++ etl.py
@@ -4,6 +4,13 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+     Load staging tables into DB. This is used to copy data from one table to another
+     
+     @param cur - DB API cursor to use
+     @param conn - DB API connection to use ( must be connected
+    """
+    # Execute all the queries in copy_table_queries.
     for query in copy_table_queries:
         print(query)
         cur.execute(query)
@@ -11,6 +18,13 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+     Insert tables into the database. This is a wrapper around : func : ` insert_table_queries ` to allow us to do a series of queries at once without having to re - execute each query in the order they were added.
+     
+     @param cur - A cursor to be used for the query.
+     @param conn - A DB API 2. 0 connection to be used
+    """
+    # Executes the insert_table_queries in the database.
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
